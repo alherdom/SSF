e6ea3f86f3f752ecf704d8d598dd81e67c7d7a51
alherdom <alejandrohd1@live.com>
2023-02-16 20:04:17 +0000

    Sync

---
+++ scripts_wd/ej02-crea_usuario.cmd
@@ -0,0 +1,18 @@
+echo off
+REM *******************************************************
+REM NOMBRE: ej02-crea_usuario.cmd
+REM OBJETIVO: Imprimir por pantalla
+REM AUTOR: Alejandro Hdez <alejandrohd1@live.com>
+REM
+REM ENTRADAS: 3
+REM SALIDAS: 4
+REM
+REM COMENTARIOS:
+REM VERSION: 1.0 (2023-01-27)
+REM
+REM ********************************************************
+
+echo Bienvenido %1
+echo Tu datos son: %1 %2
+echo Vamor a crear tu usuario %3
+echo Tu nueva ID es: %RANDOM%
---
+++ scripts_wd/ej05-calcula_segundos.cmd
@@ -0,0 +1,20 @@
+echo off
+REM *******************************************************
+REM NOMBRE: ej05-calcula_segundos.cmd
+REM OBJETIVO: Cantidad de segundos en DIAS HORAS y MINUTOS
+REM AUTOR: Alejandro Hdez <alejandrohd1@live.com>
+REM
+REM ENTRADAS: 4
+REM SALIDAS: 1
+REM
+REM COMENTARIOS:
+REM VERSION: 1.0 (2023-01-27)
+REM
+REM ********************************************************
+title calcula_segundos
+set/A dias_seg = %1 * 86400 
+set/A horas_seg =%2 * 3600
+set/A min_seg = %3 * 60
+
+set/A total_seg = %dias_seg% + %horas_seg% + %min_seg% + %4%
+echo %1 dias, %2 horas, %3 minutos y %4 segundos son %total_seg% segundos.
