92bad2a8bab61620f5895cad4491bd70e1e6f03f
alherdom <alejandrohd1@live.com>
2023-02-17 23:00:29 +0000

    Sync

--- scripts_linux/ej17-deposito.sh
+++ scripts_linux/ej17-deposito.sh
@@ -2,10 +2,10 @@
 ############################################################################
 #
 # NOMBRE: ej17-deposito.sh
-# OBJETIVO: Uso de bucles para calcular un deposito
-# AUTOR: Alejandro Hdez <alejandrohd1@live.com>
+# OBJETIVO: Calcular el rendimiento anual de un depósito bancario
+# AUTOR: Javier García <javigh1903@movistar.es> Alejandro Hdez <alejandrohd1@live.com>
 #
-# ARGUMENTOS: total=3 euros, tae, años
+# ARGUMENTOS: total=3, 1-euros, 2-%TAE, 3-años
 # SALIDAS: Devolverá la evolución del deposito por año
 #
 # FECHA:24/01/2023 
@@ -13,26 +13,24 @@
 #
 ############################################################################
 
-euros=$1
-euros_ini=$1
-tae=$2
-anyos=$3
-
 if [ $# -ne 3 ]
-   then
-   echo "No se han introducido los argumentos necesarios"
-   exit 100
-   
+then
+echo "Error de parámetros, se deben indicar 3 parámetros en el siguiente orden:
+        1 - euros depositados
+        2 - %TAE
+        3 - años"
+exit 100
 fi
-    interes=$(bc <<< "scale=2; $tae / 100")
-    result=$(bc <<< "scale=2; $euros * (1 + $interes)")
-    dif=$(bc <<< "scale=2; $result - $euros")
-
-    for ((i=1;i<=$anyos;i++))
-    do
-      echo "Año $i, deposito $result euros, has ganado en total $dif euros."
-      euros=$(bc <<< "scale=2; ($result - $euros) + $euros")
-      result=$(bc <<< "scale=2; $euros * (1 + $interes)") 
-      dif=$(bc <<< "scale=2; $result - $euros_ini")
-    done
-
+euros=$1
+TAE=$2
+anos=$3
+acumulado=0
+anterior=$euros
+echo "Mostrando los datos para un depósito de $euros euros con interés $TAE a $anos años:"
+for i in `seq 1 1 $anos`
+do
+        acumulado=`echo "scale=2; $anterior * (1 + ($TAE/100))" | bc`
+        margen=`echo "scale=2; $acumulado - $euros" | bc`
+        echo "Año $i: $anterior * ( 1 + $TAE/100) = $acumulado euros (en total has ganado $margen euros)"
+        anterior=$acumulado
+done
