7a6595d2b58994d082edd47604f7c81408a32077
alherdom <alejandrohd1@live.com>
2023-02-17 21:16:47 +0000

    Sync

--- scripts_linux/ej15-rectangulo.sh
+++ scripts_linux/ej15-rectangulo.sh
@@ -3,7 +3,7 @@
 #
 # NOMBRE: ej15-rectangulo.sh
 # OBJETIVO: Uso de bucles para pintar un rectángulo en la terminal
-# AUTOR: Alejandro Hdez <alejandrohd1@live.com>
+# AUTOR: Javier García <javigh1903@movistar.es> Alejandro Hdez <alejandrohd1@live.com>
 #
 # ARGUMENTOS: 
 # SALIDAS: Pintado del rectángulo con la altura y base correspondiente
@@ -12,28 +12,21 @@
 # VERSIONES: 1.0 (codigo inicial)
 #
 ############################################################################
-base=$1
-altura=$2
-
 if [ $# -eq 1 ]
-    then
-    altura=4
-    echo "No existe altura, se establce altura = $altura"
-    echo "Vamos a pintar un rectángulo de base: $base, altura: $altura, area: $((base * altura))"
-
-elif [ $# -ge 2 ]
-    then
-    echo "Vamos a pintar un rectángulo de base: $base, altura: $altura, area: $((base * altura))"
-
-elif [ $# -le 0 ]
-    then
-    base=7
-    altura=4
-    echo "No existen argumentos, se establece base = $base y altura = $altura"
-    echo "Vamos a pintar un rectángulo de base: $base, altura: $altura, area: $((base * altura))"
-
+  then
+  altura=4
+  echo "No existe altura, se establce altura = $altura"
+elif [ $# -eq 0 ]
+  then
+  base=7
+  altura=4
+  echo "No existen argumentos, se establece base = $base y altura = $altura"
+else
+  base=$1
+  altura=$2
 fi
 
+echo "Vamos a pintar un rectángulo de base: $base, altura: $altura, area: $((base * altura))"
 for ((j=0;j<$altura;j++))
 do
   for ((i=1;i<$base;i++))
