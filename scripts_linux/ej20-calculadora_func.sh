8fcb78e272fb1c1630d397e19d626101d4f51e6f
alherdom <alejandrohd1@live.com>
2023-02-17 23:11:53 +0000

    Sync

--- scripts_linux/ej20-calculadora_func.sh
+++ scripts_linux/ej20-calculadora_func.sh
@@ -8,56 +8,52 @@
 # ARGUMENTOS: total=2, 1-operando1, 2-operando2
 # SALIDAS: Un menú de calculadora y los resultados solicitados
 #
-# FECHA: 17/02/2023 
+# FECHA: 17/02/2023
 # VERSIONES: 1.0 (codigo inicial)
 #
 ############################################################################
 
-if [ $# -eq 0 ]
-then
-read -p "Introduzca el primer operando: " operando1
-read -p "Introduzca el segundo operando: " operando2
-elif [ $# -eq 1 ]
-then
-operando1=$1
-read -p "Introduzca el segundo operando: " operando2
+if [ $# -eq 0 ]; then
+	read -p "Introduzca el primer operando: " operando1
+	read -p "Introduzca el segundo operando: " operando2
+elif [ $# -eq 1 ]; then
+	operando1=$1
+	read -p "Introduzca el segundo operando: " operando2
 else
-operando1=$1
-operando2=$2
+	operando1=$1
+	operando2=$2
 fi
 
-suma(){
-    echo $(( $1 + $2))
+suma() {
+	echo $(($1 + $2))
 }
 
-resta(){
-    echo $(( $1 - $2 ))
+resta() {
+	echo $(($1 - $2))
 }
 
-multiplicacion(){
-    echo $(( $1 * $2 ))
+multiplicacion() {
+	echo $(($1 * $2))
 }
 
-division(){
-    if [ $2 -eq 0 ]
-                then
-                echo "División por 0, error"
-                else
-                echo $(( $1 / $2))
-                fi
+division() {
+	if [ $2 -eq 0 ]; then
+		echo "División por 0, error"
+	else
+		echo $(($1 / $2))
+	fi
 }
 
-exponenciacion(){
-    echo $(( $1 ** $2))
+exponenciacion() {
+	echo $(($1 ** $2))
 }
 
-resto_division (){
-    if [ $2 -eq 0 ]
-                      then
-                      echo "División por 0, error"
-                      else
-                      echo $(( $1 % $2))
-                      fi
+resto_division() {
+	if [ $2 -eq 0 ]; then
+		echo "División por 0, error"
+	else
+		echo $(($1 % $2))
+	fi
 }
 
 echo ""
@@ -65,7 +61,7 @@ echo "+++++++++++++++++++"
 echo "+                 +"
 echo "+   Calculadora   +"
 echo "+                 +"
-echo "+++++++++++++++++++" 
+echo "+++++++++++++++++++"
 echo ""
 
 echo "1: suma"
@@ -77,19 +73,26 @@ echo "6: resto_división"
 
 read -p "Indica la opción: " opcion
 
-case $opcion in 
-	1|suma) suma $operando1 $operando2
-	;; 
-	2|resta) resta $operando1 $operando2 
+case $opcion in
+1 | suma)
+	suma $operando1 $operando2
 	;;
-	3|multiplicación) multiplicacion $operando1 $operando2 
+2 | resta)
+	resta $operando1 $operando2
 	;;
-	4|división) division $operando1 $operando2 
-	;; 
-	5|exponenciación) exponenciacion $operando1 $operando2 
+3 | multiplicación)
+	multiplicacion $operando1 $operando2
 	;;
-	6|resto_división) resto_division $operando1 $operando2 
+4 | división)
+	division $operando1 $operando2
 	;;
-	*) echo "$opcion no es un valor válido"
+5 | exponenciación)
+	exponenciacion $operando1 $operando2
+	;;
+6 | resto_división)
+	resto_division $operando1 $operando2
+	;;
+*)
+	echo "$opcion no es un valor válido"
 	;;
 esac
