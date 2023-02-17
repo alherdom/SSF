c6d01dbc36edb360ac4d820609336956c8c0e73f
alherdom <alejandrohd1@live.com>
2023-02-17 22:41:18 +0000

    Sync

--- scripts_linux/ej09-infor_ruta.sh
+++ scripts_linux/ej09-infor_ruta.sh
@@ -3,43 +3,60 @@
 #
 # NOMBRE: ej09-info_ruta.sh
 # OBJETIVO:mostrar información de una ruta dada
-# AUTOR: Alejandro Hdez <alejandrohd1@live.com>
+# AUTOR: Javier García <javigh1903@movistar.es> Alejandro Hdez <alejandrohd1@live.com>
 #
 # ARGUMENTOS: total=1, 1:ruta
 # SALIDAS: información sobe la ruta (si existe, tipo, permisos, etc.)
 #
-# FECHA:19/01/2023 
+# FECHA: 17/01/2023 
 # VERSIOBES: 1.0 (codigo inicial)
 #
 ##############################################################################
-
 ruta=$1
 
 if [ -z "$ruta" ]
 then
 	echo "ERROR: el script $0 debe ejecutarse con un argumento (ruta)"
 	exit
-
-fi 
-
-if [ ! -e "$ruta" ]
-then 
+elif [ ! -e "$ruta" ]
+then
 	echo "La ruta '$ruta' NO existe"
 	exit
-fi
-
-if [ -d "$ruta" ]
+elif [ -d "$ruta" ]
 then
 	echo "La ruta '$ruta' es un directorio"
 elif [ -f "$ruta" ]
 then
-	echo "La ruta '$ruta' es un directorio"
+	echo "La ruta '$ruta' es un fichero"
 elif [ -h "$ruta" ]
 then
-	echo "La ruta '$ruta' es un enlace símbolico"
+	echo "La ruta '$ruta' es un enlace simbólico"
 else
 	echo "La ruta '$ruta' es de otro tipo"
 fi
 
-
-
+echo -n "Los permisos de ruta son: "
+if [ -r "$ruta" ]
+then
+	echo -n "SI lectura, "
+else
+	echo -n "NO lectura, "
+fi
+if [ -w "$ruta" ]
+then
+        echo -n "SI escritura, "
+else
+        echo -n "NO escritura, "
+fi
+if [ -x "$ruta" ]
+then
+        echo "SI ejecucion."
+else
+        echo "NO ejecucion."
+fi
+if [ -s "$ruta" ]
+then
+        echo "Tamaño de '$ruta' > 0"
+else
+        echo "Tamaño de '$ruta' = 0"
+fi
