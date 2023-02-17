7eeb973471d9ecdef64a026b4cbb84cc24bc7078
alherdom <alejandrohd1@live.com>
2023-02-17 22:38:39 +0000

    Sync

--- scripts_linux/ej07-compara.sh
+++ scripts_linux/ej07-compara.sh
@@ -3,40 +3,30 @@
 #
 # NOMBRE: ej07-compara.sh
 # OBJETIVO: Comparar dos numeros
-# AUTOR: Alejandro Hdez <alejandrohd1@live.com>
+# AUTOR: Javier García <javigh1903@movistar.es> Alejandro Hdez <alejandrohd1@live.com>
 #
 # ARGUMENTOS: total=2
 # SALIDAS: Indica que numero es mayor o si estos son iguales
 #
-# FECHA:15/02/2023 
+# FECHA: 17/02/2023 
 # VERSIONES: 1.0 (codigo inicial)
 #
 ##############################################################################
-
-num1=$1
-num2=$2
-if [ $# -lt 2 ]
-    then
-	echo "Error: No se ha introducido ningun argumento"
-        read -p "Introduce el primer numero a comparar: " num1
-	read -p "Introduce el segundo numero a comparar: " num2
-	if [ "$num1" -gt "$num2" ] && [ "$num1" -ne "0" ] 
-            then
-                echo "$num1 es mayor que $num2"
-        elif [ "$num1" -lt "$num2" ] 
-            then
-                echo "$num2 es mayor que $num1"
-        else
-                echo "$num1 es igual que $num2"
-         fi
+if [ $# -ne 2 ] 
+then
+	echo "Error: Faltan los argumentos para la comparación"
+	read -p "Introduzca el primer número: " numero1
+	read -p "Introduzca el segundo número: " numero2
 else
-	if [ "$num1" -gt "$num2" ] && [ "$num1" -ne "0" ] 
-	    then
-		echo "$num1 es mayor que $num2"
-	elif [ "$num1" -lt "$num2" ] 
-	    then
-        	echo "$num2 es mayor que $num1"
-	else
-        	echo "$num1 es igual que $num2"
-	fi
+	numero1=$1
+	numero2=$2
+fi
+if [ $numero1 -gt $numero2 ]
+then
+	echo "$numero1 es mayor que $numero2"
+elif [ $numero1 -lt $numero2 ]
+then
+	echo "$numero2 es mayor que $numero1"
+else
+	echo "Son el mismo número: $numero1"
 fi
